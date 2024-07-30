526b1949c923ab20e4f35401ca68da854624da81
Alex Kovalov <aleks.kovalyov@gmail.com>
2024-07-30 17:56:04 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "GEOSwift",
     platforms: [.iOS(.v9), .macOS("10.9"), .tvOS(.v9), .watchOS(.v2)],
     products: [
-        .library(name: "GEOSwift", targets: ["GEOSwift"])
+        .library(name: "GEOSwift", type: .dynamic, targets: ["GEOSwift"])
     ],
     dependencies: [
         .package(url: "https://github.com/GEOSwift/geos.git", from: "8.1.0")
